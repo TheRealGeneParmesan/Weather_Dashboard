0d8819efcbc3fe79e77ff95a73d8bb9c77886346
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-01-30 16:40:03 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// create global variables to store the elements in the HTML doc as well as global variable to store the API key. 
+
+var APIKey = "4b68235b6d97901e4e6eb2b454a04dd0";
+var city = "";
+var searchCity = $("#search-city");
+var searchButton = $("#search-btn");
+var selectedCity = $("#selected-city");
+var currentTemp = $("#temp");
+var currentWind = $("#wind");
+var currentHumidty = $("#humidity");
+
+
+
+
+
+
+
+
+
+
